5821bc03e55d792674dae68968617530d6e541ac
max <meq1986@163.com>
2022-04-14 15:16:29 +0800

    二数相加，利用哈希，降低时间复杂度，运行时间1ms

--- src/com/maeq/leetcode/TwoSum.java
+++ src/com/maeq/leetcode/TwoSum.java
@@ -1,6 +1,27 @@
 package com.maeq.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+
+//    class InnerClass{
+//        public int value;
+//        public int index;
+//    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            int remain = target - nums[i];
+            if(map.containsKey(remain))
+                return new int[]{i,map.get(remain)};
+            else
+                map.put(nums[i],i);
+        }
+        return null;
+    }
+
     /*
     Runtime: 84 ms
     Memory Usage: 42.3 MB
